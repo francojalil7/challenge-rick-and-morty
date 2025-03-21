47f6e54e0812813e328c3bf8fbbc052a08d3c275
francojalil <franco.jalil@zentricx.com>
2025-03-20 22:15:54 -0300

    fix: config babel

--- babel.config.js
+++ babel.config.js
@@ -4,4 +4,13 @@ module.exports = {
     ["@babel/preset-react", { runtime: "automatic" }],
     "@babel/preset-typescript",
   ],
+  env: {
+    test: {
+      presets: [
+        ["@babel/preset-env", { targets: { node: "current" } }],
+        ["@babel/preset-react", { runtime: "automatic" }],
+        "@babel/preset-typescript",
+      ],
+    },
+  },
 };
